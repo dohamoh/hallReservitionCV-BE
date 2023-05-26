1936e1ac3351653ab400be6f5d57fadbd3cf246d
Ziadal3tar <ziadalattar22@gmail.com>
2023-05-26 18:13:48 +0300

    'endInsha2allah'

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 const connection = async ()=>{
     return await mongoose.connect("mongodb+srv://ziad:00241300@cluster0.bxnbg.mongodb.net/HallRevApis")
-
     .then(()=> console.log(`connected on ...... `))
     .catch(err=>console.log(`fail to connect `))
 }
